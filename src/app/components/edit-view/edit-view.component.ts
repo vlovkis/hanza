7c971d4f17671abcf55625115c6784beb9c6eae4
vlovkis <vlovkis@inbox.lt>
2023-09-28 22:10:33 +0300

    final commit

--- src/app/components/edit-view/edit-view.component.ts
+++ src/app/components/edit-view/edit-view.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-view',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class EditViewComponent {
 
+  constructor(private router: Router ) {}
+
+  home(){
+    this.router.navigate(['/dashboard-list']);
+  }
+  settings(){
+    this.router.navigate(['/settings']);
+  }
+  logout(){
+    this.router.navigate(['/login']);
+  }
+  plasma(){
+    this.router.navigate(['/plasma-view']);
+  }
+  main(){
+    this.router.navigate(['/dashboard-detail']);
+  }
 }
